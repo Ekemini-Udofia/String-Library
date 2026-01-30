24c6e766f11aafec76defbb7ec42c49347148e25
Ekemini-Udofia <ekeminiabasiudofia@gmail.com>
2026-01-30 23:57:15 +0100

    resolved conflicts on strlen function

--- src/my-string.c
+++ src/my-string.c
@@ -1,20 +1,5 @@
 #include "my-string.h"
 #include "stdint.h"
-<<<<<<< HEAD
-size_t my_strlen(const char str[]){
-    char check = '0';
-    int count;
-    for (size_t i = 0;check!='\0'; i++)
-    {
-        check = str[i];
-        if (check!='\0')
-        {
-            count++;
-        }
-        
-    }
-    return count;
-=======
 
 
 size_t my_strlen(const char *str) {
@@ -24,5 +9,4 @@ size_t my_strlen(const char *str) {
     count++;
   } 
   return count;
->>>>>>> 3d4e19a683ac89a3c8ad2c8ca3d7996ae16bfaa9
 };
